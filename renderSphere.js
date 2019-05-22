ab00f325f6258e0f2d824844cc3a5699cb2e5928
ilsilentii <travis.vdm@outlook.com>
2019-05-21 22:23:57 -0700

    Add files via upload

--- renderSphere.js
+++ renderSphere.js
@@ -6,15 +6,27 @@ var scene = new BABYLON.Scene(engine);
  
   // Creating a camera looking to the zero point (0,0,0), a light, and a sphere of size 1
   var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
-  var light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 10), scene);
+  var light0 = new BABYLON.HemisphericLight("Omni", new BABYLON.Vector3(1, 1, 0), scene);
   //var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
 
-  BABYLON.SceneLoader.Append("./", "cube.gltf", scene, function (scene) {
+ /* BABYLON.SceneLoader.Append("./", "MatchmakingEarth.obj", scene, function (scene) {
     scene.createDefaultCameraOrLight(true, true, true);
     scene.activeCamera.alpha += Math.PI;
 	});
+*/
 
-  scene.clearColor = new BABYLON.Color3(0, 0, 0);
+	var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 8}, scene);
+
+	var materialSphere = new BABYLON.StandardMaterial("earth", scene);
+	materialSphere.diffuseTexture = new BABYLON.Texture("earth.png", scene);
+	sphere.material = materialSphere;
+
+	/*var colorSphere = new BABYLON.StandardMaterial('color', scene);
+	colorSphere.diffuseColor = new BABYLON.Color3(1,0,0);
+	sphere.material = colorSphere;*/
+
+
+  //scene.clearColor = new BABYLON.Color3(0, 0, 0);
  
   // Attach the camera to the scene
   scene.activeCamera.attachControl(canvas);
