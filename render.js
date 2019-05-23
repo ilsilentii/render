62296b999fae45b47316ef7bc0c3e592f1b44bd3
ilsilentii <travis.vdm@outlook.com>
2019-05-22 19:44:10 -0700

    Add files via upload

--- render.js
+++ render.js
@@ -5,35 +5,42 @@ var engine = new BABYLON.Engine(canvas, true);
 var scene = new BABYLON.Scene(engine);
  
   // Creating a camera looking to the zero point (0,0,0), a light, and a sphere of size 1
-  var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0,0), scene);
+  var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
+
+  camera.upVector = new BABYLON.Vector3(0.1, 0.2, 0);
+
+  camera.attachControl(canvas, true);
+
+  camera.setPosition(new BABYLON.Vector3(0, 0, -9.2));
+  camera.inputs.clear();
   var light0 = new BABYLON.HemisphericLight("Omni", new BABYLON.Vector3(1, 1, 0), scene);
   //var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
 
- /* BABYLON.SceneLoader.Append("./", "MatchmakingEarth.obj", scene, function (scene) {
-    scene.createDefaultCameraOrLight(true, true, true);
-    scene.activeCamera.alpha += Math.PI;
-	});
-*/
+ 
 
 	var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 8}, scene);
-  //sphere.rotation.x = -0.5
+  sphere.position.x = 2.9;
+  sphere.position.y = -1
 
 	var materialSphere = new BABYLON.StandardMaterial("earth", scene);
 	materialSphere.diffuseTexture = new BABYLON.Texture("earth.png", scene);
 	sphere.material = materialSphere;
 
-	/*var colorSphere = new BABYLON.StandardMaterial('color', scene);
-	colorSphere.diffuseColor = new BABYLON.Color3(1,0,0);
-	sphere.material = colorSphere;*/
 
+  camera.rotation.y = 2
 
-  //scene.clearColor = new BABYLON.Color3(0, 0, 0);
+  scene.clearColor = new BABYLON.Color3(0, 0, 0);
  
-  // Attach the camera to the scene
-  //scene.activeCamera.attachControl(canvas);
+  
  
   // Once the scene is loaded, just register a render loop to render it
   engine.runRenderLoop(function () {
     scene.render();
-    sphere.rotation.y += -0.005;
+    //sphere.rotation.y += -0.0005;
+    sphere.rotate(BABYLON.Axis.Y, -0.0005, BABYLON.Space.WORLD);
+    
+    /*var url = "https://upload.wikimedia.org/wikipedia/commons/e/e9/Gradient_1024x768.jpg";
+    var background = new BABYLON.Layer("back", url, scene);
+  background.isBackground = true;*/
+
   });
